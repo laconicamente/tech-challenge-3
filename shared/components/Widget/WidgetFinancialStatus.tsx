fb11f8e79c4b6b02d2b0346bdff1869c801d32a4
Daniela Farias <daniela.pereira@picpay.com>
2025-09-24 13:45:45 -0300

    feat: add new status

--- shared/components/Widget/WidgetFinancialStatus.tsx
+++ shared/components/Widget/WidgetFinancialStatus.tsx
@@ -13,6 +13,14 @@ const WidgetFinancialStatus = () => {
 
     const isBalancePositive = balanceValue >= 0;
 
+    if (balanceValue === 0 && !isLoading) return (<View style={{ paddingTop: 5 }}>
+            {isLoading ? (<LoadingSkeleton />) :
+                (<View style={{ width: '100%', backgroundColor: ColorsPalette.light['grey.800'], padding: 20, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', borderRadius: 20 }}>
+                    <Text style={{ fontSize: 16, color: '#fefefe', textAlign: 'center' }}>Status financeiro</Text>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#fff', textAlign: 'center' }}>NEUTRO</Text>
+                </View>)}
+        </View>);
+
     return (
         <View style={{ paddingTop: 5 }}>
             {isLoading ? (<LoadingSkeleton />) :
